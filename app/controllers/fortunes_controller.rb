19a86b833b1969351b7a4efc6dd5197524b5b95a
mayuken <superbeaver.drum@gmail.com>
2020-04-09 19:46:29 +0900

    back_end_fix
    # WHAT
    ・ニックネームに入力された文字数を算出
    ・誕生日に入力された数字を1桁ずつ足す
    ・性別に入力された文字数を算出
    ・好きな物に入力された文字数を算出
    ・当日の日付の和を算出
    ・上記全ての和を算出

---
+++ app/controllers/fortunes_controller.rb
@@ -0,0 +1,56 @@
+class FortunesController < ApplicationController
+  def index
+  end
+
+  def get_digits(n)
+    n.abs.to_s.each_byte.map{|b| b - 0x30}
+  end
+
+  def result
+    # ニックネームの文字数
+    name_math = params[:name].length
+
+    # 入力された文字列から"-"を除外して数値化
+    birthday_i = params[:birthday].delete("-")
+    # 数値化した誕生日を1桁ずつ配列に入れる
+    birthday_array = birthday_i.chars.map(&:to_i)
+    # 配列の値を全て足す
+    birthday_math = birthday_array.inject(:*)
+
+    # 性別の文字数
+    gender_math = params[:gender].length
+
+    # 好きな物の文字数
+    like_math = params[:like].length
+
+    # 当日の日付を取得
+    require 'date'
+    date = Date.today.to_time
+    # 当日の日付を1桁ずつ足す
+    date_math = date.to_s.slice(0..9).delete("-").chars.map(&:to_i).inject(:+)
+
+    result = name_math + birthday_math + gender_math + like_math + date_math
+
+    if result % 27 == 0
+      @fortune_image = "/omikuzi_image/omikuji_daikyou.png"
+    elsif result % 23 == 0
+      @fortune_image = "/omikuzi_image/omikuji_kyou.png"
+    elsif result % 7 == 0
+      @fortune_image = "/omikuzi_image/omikuji_daikichi.png"
+    elsif result % 6 == 0
+      @fortune_image = "/omikuzi_image/omikuji_kichi.png"
+    elsif result % 5 == 0
+      @fortune_image = "/omikuzi_image/omikuji_chuukichi.png"
+    elsif result % 4 == 0
+      @fortune_image = "/omikuzi_image/omikuji_suekichi.png"
+    elsif result % 3 == 0
+      @fortune_image = "/omikuzi_image/omikuji_syoukichi.png"
+    else
+      @fortune_image = "/omikuzi_image/omikuji_daikichi.png"
+    end
+
+  end
+
+
+  
+end
